9393f0a25ca16b2e665b321771bf001c2f4cde9e
Steven Scott <stevescott2653@gmail.com>
2019-10-02 16:42:53 -0400

    Add files via upload

---
+++ Sorted_List/Sorted_List/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Sorted_List
+{
+    public class SortedSetExample
+    {
+        public static void Main(string[] args)
+        {
+            // create a set of strings
+            var names = new SortedSet<string>();
+            names.Add("Joe");
+            names.Add("Bob");
+            names.Add("Tim");
+            names.Add("Tom");
+            names.Add("Joe"); // will not be added
+
+            // Iterate SortedSet elements using for each loop
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
